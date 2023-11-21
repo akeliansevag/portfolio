b6c722dfef8e84e5d90fd97586105b877a30ef7b
akeliansevag <akelian.sevag@gmail.com>
2023-11-21 13:14:16 +0200

    added stop propagation

--- app/components/homepage/portfolio/Popup.js
+++ app/components/homepage/portfolio/Popup.js
@@ -5,8 +5,8 @@ import React from 'react';
 const Popup = ({item,isPopupOpen, handleClosePopup}) => {
   return (
     <div className={`overflow-hidden transition-colors duration-300 fixed w-full bg-black top-0 left-0 z-10 ${isPopupOpen ? 'bg-opacity-80 h-full' : 'bg-opacity-0 h-0'}`}>
-        <div className='p-4 w-full h-full overflow-auto flex items-center justify-center'>
-            <div className={`relative p-3 bg-white w-full max-w-2xl transition-all duration-300 ease-in-out ${isPopupOpen ? 'translate-y-0 opacity-1' : 'translate-y-5 opacity-0'}`}>
+        <div onClick={handleClosePopup} className='p-4 w-full h-full overflow-auto flex items-center justify-center'>
+            <div onClick={(e)=> e.stopPropagation()} className={`relative p-3 bg-white w-full max-w-2xl transition-all duration-300 ease-in-out ${isPopupOpen ? 'translate-y-0 opacity-1' : 'translate-y-5 opacity-0'}`}>
                 <div onClick={handleClosePopup} className='px-3 py-1 text-3xl absolute top-0 right-0 z-10 bg-white hover:cursor-pointer'>x</div>
                 <div className='aspect-video relative'>
                     <Image sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw" className='object-cover' alt='Portfolio Image' src={`/${item.image}`} fill={true} />
