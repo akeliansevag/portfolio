1098f33c1ede1c81a56df8ec48bc30a37f96f82b
akeliansevag <akelian.sevag@gmail.com>
2023-11-23 15:33:07 +0200

    ,

--- app/api/useFetch.js
+++ app/api/useFetch.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import { useState, useEffect, useRef } from 'react';
 
-const useFetch = () => {
-    return (
-        <div>
+const useFetch = (url) => {
+    const isInitialMount = useRef(true);
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-        </div>
-    )
-}
+    useEffect(() => {
+        setError(null);
+        setLoading(true);
+        if (isInitialMount.current) {
+            isInitialMount.current = false;
+            return;
+        }
+
+        const fetchData = async () => {
+            try {
+                const response = await fetch(url);
+                // Check if the response status is OK (200)
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                const result = await response.json();
+                setData(result);
+            } catch (err) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [url]);
+
+    return { data, error, loading };
+};
+
 export default useFetch;
