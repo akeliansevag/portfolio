ddefc7226a3a7230a39c3e0c6b16e187dcc48928
akeliansevag <akelian.sevag@gmail.com>
2023-11-28 22:58:11 +0200

    .

--- app/components/SocialIcons.js
+++ app/components/SocialIcons.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import * as ReactIcons from "react-icons/fa";
 
-import Link from 'next/link';
 import useFetch from '../api/useFetch';
 import { SOCIAL_MEDIAS_API } from '../api/api';
 import Skeleton from './skeletons/Skeleton';
@@ -16,9 +15,9 @@ const SocialIcons = () => {
                 data?.data.map((icon) => {
                     let IconComponent = ReactIcons[icon.icon];
                     return (
-                        <Link key={icon.id} className='flex items-center justify-center text-white bg-primary text-lg w-[40px] h-[40px] rounded-full hover:bg-secondary' target='_blank' href={icon.link}>
+                        <a key={icon.id} className='flex items-center justify-center text-white bg-primary text-lg w-[40px] h-[40px] rounded-full hover:bg-secondary' target='_blank' href={icon.link}>
                             {IconComponent && <IconComponent />}
-                        </Link>
+                        </a>
                     )
                 })
             )
