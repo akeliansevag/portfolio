0ca3cc00626f290f928026b3a4672cf86ea10335
akeliansevag <akelian.sevag@gmail.com>
2023-11-28 22:14:15 +0200

    ,

--- app/components/homepage/Skills.js
+++ app/components/homepage/Skills.js
@@ -8,99 +8,100 @@ import { SiCakephp, SiTailwindcss } from "react-icons/si";
 import { TbBrandReactNative } from "react-icons/tb";
 import { MdAnimation } from "react-icons/md";
 
-const skills = [
-    {
-        id: 1,
-        icon: <FaHtml5 />,
-        title: 'HTML5',
-    },
-    {
-        id: 2,
-        icon: <FaCss3 />,
-        title: 'CSS3'
-    },
-    {
-        id: 3,
-        icon: <IoLogoJavascript />,
-        title: 'JavaScript'
-    },
-    {
-        id: 4,
-        icon: <DiJqueryLogo />,
-        title: 'JQuery'
-    },
-    {
-        id: 5,
-        icon: <SiCakephp />,
-        title: 'CakePHP, Croogo'
-    },
-    {
-        id: 6,
-        icon: <FaReact />,
-        title: 'React'
-    },
-    {
-        id: 7,
-        icon: <TbBrandReactNative />,
-        title: 'React Native'
-    },
-    {
-        id: 8,
-        icon: <FaWordpress />,
-        title: 'WordPress'
-    },
-    {
-        id: 9,
-        icon: <DiMysql />,
-        title: 'MySQL'
-    },
-    {
-        id: 10,
-        icon: <FaPhp />,
-        title: 'PHP'
-    },
-    {
-        id: 11,
-        icon: <MdAnimation />,
-        title: 'GSAP'
-    },
-    {
-        id: 12,
-        icon: <FaGithub />,
-        title: 'GitHub'
-    },
-    {
-        id: 13,
-        icon: <FaCode />,
-        title: 'Rest API'
-    },
-    {
-        id: 14,
-        icon: <SiTailwindcss />,
-        title: 'Tailwind CSS'
-    },
-    {
-        id: 15,
-        icon: <FaBootstrap />,
-        title: 'Bootstrap'
-    },
-    {
-        id: 16,
-        icon: <FaLaravel />,
-        title: 'Laravel'
-    },
-    {
-        id: 17,
-        icon: <FaCode />,
-        title: 'Technical SEO'
-    },
-    {
-        id: 18,
-        icon: <DiResponsive />,
-        title: 'Responsive Design'
-    }
-];
+
 const Skills = () => {
+    const skills = [
+        {
+            id: 1,
+            icon: <FaHtml5 />,
+            title: 'HTML5',
+        },
+        {
+            id: 2,
+            icon: <FaCss3 />,
+            title: 'CSS3'
+        },
+        {
+            id: 3,
+            icon: <IoLogoJavascript />,
+            title: 'JavaScript'
+        },
+        {
+            id: 4,
+            icon: <DiJqueryLogo />,
+            title: 'JQuery'
+        },
+        {
+            id: 5,
+            icon: <SiCakephp />,
+            title: 'CakePHP, Croogo'
+        },
+        {
+            id: 6,
+            icon: <FaReact />,
+            title: 'React'
+        },
+        {
+            id: 7,
+            icon: <TbBrandReactNative />,
+            title: 'React Native'
+        },
+        {
+            id: 8,
+            icon: <FaWordpress />,
+            title: 'WordPress'
+        },
+        {
+            id: 9,
+            icon: <DiMysql />,
+            title: 'MySQL'
+        },
+        {
+            id: 10,
+            icon: <FaPhp />,
+            title: 'PHP'
+        },
+        {
+            id: 11,
+            icon: <MdAnimation />,
+            title: 'GSAP'
+        },
+        {
+            id: 12,
+            icon: <FaGithub />,
+            title: 'GitHub'
+        },
+        {
+            id: 13,
+            icon: <FaCode />,
+            title: 'Rest API'
+        },
+        {
+            id: 14,
+            icon: <SiTailwindcss />,
+            title: 'Tailwind CSS'
+        },
+        {
+            id: 15,
+            icon: <FaBootstrap />,
+            title: 'Bootstrap'
+        },
+        {
+            id: 16,
+            icon: <FaLaravel />,
+            title: 'Laravel'
+        },
+        {
+            id: 17,
+            icon: <FaCode />,
+            title: 'Technical SEO'
+        },
+        {
+            id: 18,
+            icon: <DiResponsive />,
+            title: 'Responsive Design'
+        }
+    ];
     return (
         <section className='mt-16'>
             <div className='container'>
